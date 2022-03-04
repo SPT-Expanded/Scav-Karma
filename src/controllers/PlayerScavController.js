26163a6c0a3861775799d17937ec10847a711b25
citharus <citharus@protonmail.com>
2022-03-04 19:36:36 +0100

    Added PlayerScav controller

---
+++ src/controllers/PlayerScavController.js
@@ -0,0 +1,67 @@
+"use strict";
+
+class PlayerScavController {
+    static generateScav(sessionId) {
+        const pmcData = ProfileController.getPmcProfile(sessionId);
+        let bot = JsonUtil.clone(DatabaseServer.tables.bots.base);
+        let node = JsonUtil.clone(DatabaseServer.tables.bots.types.assault);
+
+        const scavKarma = pmcData.TradersInfo["579dc571d53a0658a154fbec"].standing;
+        node = PlayerScavController.modifyBotNode(node, scavKarma)
+
+        bot.Info.Settings.BotDifficulty = "normal";
+        bot.Info.Settings.Role = "assault";
+        bot.Info.Side = "Savage";
+        bot.Info.Nickname = `${RandomUtil.getArrayValue(node.firstName)} ${RandomUtil.getArrayValue(node.lastName) || ""}`;
+
+        if (BotConfig.showTypeInNickname) {
+            bot.Info.Nickname += ` ${bot.Info.Settings.Role}`;
+        };
+
+        bot.Info.Settings.StandingForKill = node.experience.standingForKill;
+        bot.Info.Voice = RandomUtil.getArrayValue(node.appearance.voice);
+        bot.Health = BotController.generateHealth(node.health, bot.Info.Side === "Savage");
+        bot.Skills = BotController.generateSkills(node.skills);
+        bot.Customization.Head = RandomUtil.getArrayValue(node.appearance.head);
+        bot.Customization.Body = RandomUtil.getArrayValue(node.appearance.body);
+        bot.Customization.Feet = RandomUtil.getArrayValue(node.appearance.feet);
+        bot.Customization.Hands = RandomUtil.getArrayValue(node.appearance.hands);
+        bot.Inventory = BotGenerator.generateInventory(node.inventory, node.chances, node.generation);
+        Logger.warning(bot.Inventory);
+
+        bot = InventoryHelper.generateInventoryID(bot);
+
+        bot._id = pmcData.savage;
+        bot.aid = sessionId;
+        bot.Info.Settings = {};
+        bot.TradersInfo = JsonUtil.clone(pmcData.TradersInfo);
+        bot.Skills = ProfileController.getScavSkills(sessionId);
+        bot.Stats = ProfileController.getScavStats(sessionId);
+        bot.Info.Level = ProfileController.getScavLevel(sessionId);
+        bot.Info.Experience = ProfileController.getScavExperience(sessionId);
+
+        bot = InventoryHelper.removeSecureContainer(bot);
+        bot = ProfileController.setScavCooldownTimer(bot, pmcData);
+
+        ProfileController.setScavProfile(sessionId, bot);
+        return bot;
+    };
+
+    static modifyBotNode(node, scavKarma) {
+        const config = require("../../config/config.json");
+
+        scavKarma = Math.round(scavKarma) || 1;
+        const overwriteConfig = require(`../../config/${scavKarma}.json`);
+
+        if (config.changeItemGeneration) {
+            node.generation = overwriteConfig.generation;
+        } else if (config.changeChances.equipment) {
+            node.chances.equipment = overwriteConfig.chances.equipment;
+        } else if (config.changeChances.mods) {
+            node.chances.mods = overwriteConfig.chances.mods;
+        };
+        return node;
+    };
+};
+
+module.exports = PlayerScavController;
