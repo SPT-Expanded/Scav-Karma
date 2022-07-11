8ad46f85d190988c3ad519c2830d3b02efad79dd
citharus <citharus@protonmail.com>
2022-07-11 17:51:46 +0200

    Added overwrite for `generatePlayerScav`

--- src/mod.ts
+++ src/mod.ts
@@ -2,6 +2,8 @@ import {DependencyContainer} from "tsyringe";
 import {IMod} from "@spt-aki/models/external/mod";
 import {ProfileController} from "@spt-aki/controllers/ProfileController";
 
+import {PlayerScavController} from "./controllers/PlayerScavController";
+
 class Mod implements IMod {
     private config = require("../config/config.json");
 
@@ -9,6 +11,11 @@ class Mod implements IMod {
         if (!this.config.enableMod) {
             return;
         }
+        container.afterResolution("ProfileController", (_t, result: ProfileController) => {
+            result.generatePlayerScav = (sessionID: string) => {
+                return new PlayerScavController(container).generatePlayerScav(sessionID);
+            }
+        }, {frequency: "Always"});
     }
 
     public delayedLoad(container: DependencyContainer): void {
