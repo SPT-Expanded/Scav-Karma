f1088b43da887252eef8f1931eb9cfc7e2d79415
citharus <citharus@protonmail.com>
2022-07-25 01:01:11 +0200

    Added loading message

--- src/mod.ts
+++ src/mod.ts
@@ -4,17 +4,22 @@ import {StaticRouterModService} from "@spt-aki/services/mod/staticRouter/StaticR
 import {HttpResponseUtil} from "@spt-aki/utils/HttpResponseUtil";
 
 import {PlayerScavGenerator} from "./generators/PlayerScavGenerator";
+import {ILogger} from "@spt-aki/models/spt/utils/ILogger";
 
 class Mod implements IPreAkiLoadMod {
     private config = require("../config/config.json");
+    private package = require("../package.json");
 
     public preAkiLoad(container: DependencyContainer): void {
-        const staticRouterModService = container.resolve<StaticRouterModService>("StaticRouterModService");
-        const httpResponse = container.resolve<HttpResponseUtil>("HttpResponseUtil");
-
         if (!this.config.enableMod) {
             return;
         }
+
+        const staticRouterModService = container.resolve<StaticRouterModService>("StaticRouterModService");
+        const httpResponse = container.resolve<HttpResponseUtil>("HttpResponseUtil");
+        const logger = container.resolve<ILogger>("WinstonLogger");
+
+        logger.info(`Loading: ${this.package.displayName}`);
 
         staticRouterModService.registerStaticRouter(
             "StaticRouteRegeneratePlayerScav",
